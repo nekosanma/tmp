e4392fb1fb40610223fb445c313ad3848dc4e107
nekosanma <nekosanma999@gmail.com>
2017-08-15 12:53:44 +0900

    Update TextUtil.java

--- TextUtil.java
+++ TextUtil.java
@@ -6,19 +6,19 @@ import java.util.List;
 
 public class TextUtil {
 
-	public static List<String> read(File file) {
-		List<String> list = new ArrayList<String>();
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(file));
-			String str;
-			while ((str = br.readLine()) != null) {
-				list.add(str);
-			}
-			br.close();
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-		return list;
-	}
+  public static List<String> read(File file) {
+    List<String> list = new ArrayList<String>();
+    try {
+      BufferedReader br = new BufferedReader(new FileReader(file));
+      String str;
+      while ((str = br.readLine()) != null) {
+        list.add(str);
+      }
+      br.close();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    return list;
+  }
 
 }
